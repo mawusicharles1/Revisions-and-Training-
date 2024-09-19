218b1d929e4c8c3d757456a480ca400d854d1511
mawusicharles1 <mawusicharles@yahoo.com>
2024-09-19 11:23:35 +0100

    sql prep

--- sql_practice.sql
+++ sql_practice.sql
@@ -4,8 +4,8 @@ firstname CHAR (20),
 lastname CHAR(30), 
 Age INT (3), 
 email CHAR (100) 
-
-); 
-
+);
 INSERT INTO team (firstname, lastname, age, email)
-VALUES (" Charles", " famous", 38, "mawusicharles@yahoo.com") ;
+VALUES (" Charles", " famous", 38, "mawusicharles@yahoo.com");
+
+SELECT * FROM team ;
