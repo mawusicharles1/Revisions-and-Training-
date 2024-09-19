9888824b979f5aa91779447659ae31f20a77a6ca
mawusicharles1 <mawusicharles@yahoo.com>
2024-09-19 10:59:44 +0100

    creating_table_prep

---
+++ sql_practice.sql
@@ -0,0 +1,11 @@
+DROP TABLE TEAM ;
+CREATE TABLE team (
+firstname CHAR (20),
+lastname CHAR(30), 
+Age INT (3), 
+email CHAR (100) 
+
+); 
+
+INSERT INTO team (firstname, lastname, age, email)
+VALUES (" Charles", " famous", 38, "mawusicharles@yahoo.com") ;
